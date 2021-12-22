5417288b1ed10d785fb237ea01580d2be5ed2b86
docerm <68264652+docerm@users.noreply.github.com>
2021-12-22 13:33:51 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=788507ac-9306-468d-b460-6c84e413608a
+AUUID=588507ac-9306-468d-b490-6c84e413608a
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
